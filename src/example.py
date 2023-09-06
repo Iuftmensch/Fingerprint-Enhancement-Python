b4f52e71c15b1ae834dfd8dc720a1e225e57ee42
Iuftmensch <1102324554@qq.com>
2023-09-06 16:29:12 -0400

    process the images in a batch

--- src/example.py
+++ src/example.py
@@ -6,22 +6,31 @@ Created on Mon Apr 18 11:42:58 2016
 """
 
 import sys
-from src.FingerprintImageEnhancer import FingerprintImageEnhancer
+from FingerprintImageEnhancer import FingerprintImageEnhancer
 import cv2
+import os #this package makes the fingerprint_enhancer can process the images in a batch
 
 if __name__ == '__main__':
 
     image_enhancer = FingerprintImageEnhancer()         # Create object called image_enhancer
-    if(len(sys.argv)<2):                                # load input image
-        print('loading sample image');
-        img_name = '2.jpg'
-        img = cv2.imread('../images/' + img_name)
-    elif(len(sys.argv) >= 2):
-        img_name = sys.argv[1];
-        img = cv2.imread('../images/' + img_name)
 
-    if(len(img.shape)>2):                               # convert image into gray if necessary
-         img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    image_folder_path = '../images/' # create the folder path for the input images
+    enhanced_folder_path = '../enhanced/' #create the folder path for the output enhanced images
 
-    out = image_enhancer.enhance(img)     # run image enhancer
-    image_enhancer.save_enhanced_image('../enhanced/' + img_name)   # save output
+    print("load images...")
+    for img_name in os.listdir(image_folder_path): #loop through all the images in the input image folder
+        print(f'Processing {img_name}...')
+
+        img_path = os.path.join(image_folder_path, img_name)
+        img = cv2.imread(img_path)
+        if(len(img.shape)>2):                               # convert image into gray if necessary
+            img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+        out = image_enhancer.enhance(img)     # run image enhancer
+
+        filename, file_extension = os.path.splitext(img_name) # enhanced images
+        enhanced_img_name = f"{filename}_enhanced{file_extension}"
+
+        enhanced_img_path = os.path.join(enhanced_folder_path, enhanced_img_name)
+        image_enhancer.save_enhanced_image(enhanced_img_path)   # save output
+    print("save result...")
